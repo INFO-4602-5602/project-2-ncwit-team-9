6e6c521cd16633dca640d9999457a6b9a7c57659
Ashwin Sankar <ashwinoscar@gmail.com>
2018-03-15 14:56:42 -0600

    gender select

--- viz2/main.js
+++ viz2/main.js
@@ -1,16 +1,84 @@
-
-
+//filters
 var select_hsgpa = 3.2;
 var select_gender = "all";
 var select_inst = "all";
 
+//dropdown option
+var dropdown_options = [{
+    value: "all",
+    text: "All"
+  },
+  {
+    value: "487.0",
+    text: "487"
+  },
+  {
+    value: "287.0",
+    text: "287"
+  },
+  {
+    value: "13.0",
+    text: "13"
+  },
+  {
+    value: "194.0",
+    text: "194"
+  },
+  {
+    value: "182.0",
+    text: "182"
+  },
+  {
+    value: "373.0",
+    text: "373"
+  },
+  {
+    value: "319.0",
+    text: "319"
+  },
+  {
+    value: "115.0",
+    text: "115"
+  },
+  {
+    value: "85.0",
+    text: "85"
+  },
+  {
+    value: "157.0",
+    text: "157"
+  },
+  {
+    value: "489.0",
+    text: "489"
+  },
+  {
+    value: "324.0",
+    text: "324"
+  },
+  {
+    value: "361.0",
+    text: "361"
+  },
+  {
+    value: "249.0",
+    text: "249"
+  },
+  {
+    value: "205.0",
+    text: "205"
+  }
+
+];
+
 
 var tooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
+  .attr("class", "tooltip1")
   .style("visibility", "hidden");
 
+//margins for d3
 var margin = {
-    top: 20,
+    top: 30,
     right: 20,
     bottom: 30,
     left: 30
@@ -18,22 +86,54 @@ var margin = {
   width = 350 - margin.left - margin.right,
   height = 350 - margin.top - margin.bottom;
 
+
+//slider reversing the value
 var slider = new Slider("#ex3", {
   reversed: true
 });
 
+//dropdown filling
+d3.select("#inst_dropdown ")
+  .selectAll("option")
+  .data(dropdown_options)
+  .enter()
+  .append("option")
+  .attr("value", function(option) {
+    return option.value;
+  })
+  .text(function(option) {
+    return option.text;
+  });
 
 
+
+//slider listner
 slider.on("slide", function(sliderValue) {
   select_hsgpa = sliderValue;
   populateBin(select_inst, select_gender, select_hsgpa);
 });
 
-d3.selectAll("input[name='stack']").on("change", function(){
-    select_gender = this.value;
-    populateBin(select_inst, select_gender, select_hsgpa);
+//gender selector
+d3.selectAll("input[name='stack']").on("change", function() {
+  select_gender = this.value;
+  populateBin(select_inst, select_gender, select_hsgpa);
 });
 
+//institution change
+d3.select("#inst_dropdown ")
+  .on("change", function() {
+    select_inst = d3.event.target.value;
+    populateBin(select_inst, select_gender, select_hsgpa);
+  })
+
+
+//css : baby blue : #89cff0  baby pink : #FFB6C1
+// green base : #9FDD9F
+
+//viz 2 : error plot bar
+/*
+input: filters for the data
+*/
 function populateBin(typeInst, typeGender, typehsgpa) {
   console.log(typeGender);
   d3.json('../data.json', function(error, data) {
@@ -46,13 +146,10 @@ function populateBin(typeInst, typeGender, typehsgpa) {
 
     var tickLabels = ["", "freshman", "sophomore", "junior", "senior"];
 
-    console.log(data);
-
     d3.select("#partTwo")
       .data(data.filter(function(d) {
         if (d.inst == typeInst && d.gender == typeGender && d.hs_gpa == typehsgpa) {
-          // hs_gpa_list.push(d.hs_gpa);
-          // freshman_list.push(d.avg);
+
           yscale_val.push({
             x: count,
             y: d.avg,
@@ -87,15 +184,14 @@ function populateBin(typeInst, typeGender, typehsgpa) {
         return yScale(d.y);
       });
 
-    if(d3.select("svg"))
-    {
+    if (d3.select("svg")) {
       d3.select("svg").remove();
     }
-      var svg = d3.select("#plot").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("#plot").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 
@@ -190,11 +286,47 @@ function populateBin(typeInst, typeGender, typehsgpa) {
       .attr("y", (margin.top / 2))
       .attr("text-anchor", "middle")
       .style("font-size", "16px")
-      .text("Inst : " + typeInst + "| Gender: " + typeGender + "| hs_gpa: " + typehsgpa);
+      .text("Inst : " + typeInst + " | Gender: " + typeGender + " | hs_gpa: " + typehsgpa);
+
+    if(select_gender == "female")
+    {
+      svg.selectAll("circle")
+      .style("fill","#FFB6C1");
+
+      svg.selectAll(".error-cap")
+      .style('stroke','#D64F63');
+
+      svg.selectAll(".error-line")
+      .style('stroke','#b30059');
+    }
+    else if(select_gender == "male")
+    {
+      svg.selectAll("circle")
+      .style("fill","#89cff0");
+
+      svg.selectAll(".error-cap")
+      .style('stroke','#2A7598');
+
+      svg.selectAll(".error-line")
+      .style('stroke','#00496B');
+    }
+
+    else if(select_gender == "all")
+    {
+      svg.selectAll("circle")
+      .style("fill","#9FDD9F");
+
+
+      svg.selectAll(".error-cap")
+      .style('stroke','#30ae30');
+
+      svg.selectAll(".error-line")
+      .style('stroke','#037c03');
+    }
 
   });
 
 
 }
 
-populateBin("all", "all", "3.2");
+populateBin("all", "all", "3.5");
