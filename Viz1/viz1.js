1de80bf9ac3dbf4835e8941fb13874f53e216d85
supriyanaidu <32039542+supriyanaidu@users.noreply.github.com>
2018-03-16 00:38:22 -0600

    fixed partial donut

--- Viz1/viz1.js
+++ Viz1/viz1.js
@@ -1,10 +1,11 @@
 var barChartData = [];
+var donutChartData = [];
 var select_gender = "All";
 
 var tooltip = d3.select("body").append("div")
   .attr("class", "tooltip")
   .style("visibility", "hidden");
-
+var 	formatAsPercentage = d3.format("%");
 var margin = {
     top: 20,
     right: 20,
@@ -21,8 +22,7 @@ var margin = {
 	 dsBarChart(select_gender);
 	});
 
-	function dsPieChart(){
-
+	function dsDonutChart(){
 		var 	width = 400,
 			   height = 400,
 			   outerRadius = Math.min(width, height) / 2,
@@ -30,75 +30,75 @@ var margin = {
 			   // for animation
 			   innerRadiusFinal = outerRadius * .5,
 			   innerRadiusFinal3 = outerRadius* .45,
-			   color = d3.schemeCategory20b();   //builtin range of colors
+			   color = d3.scaleOrdinal(d3.schemeCategory20b);   //builtin range of colors
 
 
-		var vis = d3.select("#pieChart")
+		var vis = d3.select("#donut")
 		     .append("svg:svg")              //create the SVG element inside the <body>
-		     .data([dataset])                   //associate our data with the document
+		     .data(donutChartData)                   //associate our data with the document
 		         .attr("width", width)           //set the width and height of our visualization (these will be attributes of the <svg> tag
 		         .attr("height", height)
-		     		.append("svg:g")                //make a group to hold our pie chart
-		         .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")    //move the center of the pie chart from 0, 0 to radius, radius
-					;
+						 .append("svg:g")                //make a group to hold our pie chart
+ 	         .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+ 				;
 
-	   var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
-	        	.outerRadius(outerRadius).innerRadius(innerRadius);
+    var arc = d3.arc()              //this will create <path> elements for us using arc data
+         	.outerRadius(outerRadius).innerRadius(innerRadius);
 
-	   // for animation
-	   var arcFinal = d3.svg.arc().innerRadius(innerRadiusFinal).outerRadius(outerRadius);
-		var arcFinal3 = d3.svg.arc().innerRadius(innerRadiusFinal3).outerRadius(outerRadius);
+    // for animation
+    var arcFinal = d3.arc().innerRadius(innerRadiusFinal).outerRadius(outerRadius);
+ 	var arcFinal3 = d3.arc().innerRadius(innerRadiusFinal3).outerRadius(outerRadius);
 
-	   var pie = d3.layout.pie()           //this will create arc data for us given a list of values
-	        .value(function(d) { return d.measure; });    //we must tell it out to access the value of each element in our data array
+    var pie = d3.pie()           //this will create arc data for us given a list of values
+         .value(function(d) { return d.measure; });    //we must tell it out to access the value of each element in our data array
 
-	   var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
-	        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
-	        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-	            .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
-	               .attr("class", "slice")    //allow us to style things in the slices (like text)
-	               .on("mouseover", mouseover)
-	    				.on("mouseout", mouseout)
-	    				.on("click", up)
-	    				;
+    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+         .data(pie(donutChartData))                        //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
+         .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+             .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+                .attr("class", "slice")    //allow us to style things in the slices (like text)
+                .on("mouseover", mouseover)
+     				.on("mouseout", mouseout)
+     				// .on("click", up)
+     				;
 
-	        arcs.append("svg:path")
-	               .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
-	               .attr("d", arc)     //this creates the actual SVG path using the associated data (pie) with the arc drawing function
-						.append("svg:title") //mouseover title showing the figures
-					   .text(function(d) { return d.data.category + ": " + formatAsPercentage(d.data.measure); });
+         arcs.append("svg:path")
+                .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+                .attr("d", arc)     //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+ 					.append("svg:title") //mouseover title showing the figures
+ 				   .text(function(d) { return d.data.race + ": " + formatAsPercentage(d.data.measure); });
 
-	        d3.selectAll("g.slice").selectAll("path").transition()
-				    .duration(750)
-				    .delay(10)
-				    .attr("d", arcFinal )
-				    ;
+         d3.selectAll("g.slice").selectAll("path").transition()
+ 			    .duration(750)
+ 			    .delay(10)
+ 			    .attr("d", arcFinal )
+ 			    ;
 
-		  // Add a label to the larger arcs, translated to the arc centroid and rotated.
-		  // source: http://bl.ocks.org/1305337#index.html
-		  arcs.filter(function(d) { return d.endAngle - d.startAngle > .2; })
-		  		.append("svg:text")
-		      .attr("dy", ".35em")
-		      .attr("text-anchor", "middle")
-		      .attr("transform", function(d) { return "translate(" + arcFinal.centroid(d) + ")rotate(" + angle(d) + ")"; })
-		      //.text(function(d) { return formatAsPercentage(d.value); })
-		      .text(function(d) { return d.data.category; })
-		      ;
+ 	  // Add a label to the larger arcs, translated to the arc centroid and rotated.
+ 	  // source: http://bl.ocks.org/1305337#index.html
+ 	  arcs.filter(function(d) { return d.endAngle - d.startAngle > .2; })
+ 	  		.append("svg:text")
+ 	      .attr("dy", ".35em")
+ 	      .attr("text-anchor", "middle")
+ 	      .attr("transform", function(d) { return "translate(" + arcFinal.centroid(d) + ")rotate(" + angle(d) + ")"; })
+ 	      //.text(function(d) { return formatAsPercentage(d.value); })
+ 	      .text(function(d) { return d.data.race; })
+ 	      ;
 
-		   // Computes the label angle of an arc, converting from radians to degrees.
-			function angle(d) {
-			    var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
-			    return a > 90 ? a - 180 : a;
-			}
+ 	   // Computes the label angle of an arc, converting from radians to degrees.
+ 		function angle(d) {
+ 		    var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
+ 		    return a > 90 ? a - 180 : a;
+ 		}
 
 
-			// Pie chart title
-			vis.append("svg:text")
-		     	.attr("dy", ".35em")
-		      .attr("text-anchor", "middle")
-		      .text("Demographic Enrollment")
-		      .attr("class","title")
-		      ;
+ 		// Pie chart title
+ 		vis.append("svg:text")
+ 	     	.attr("dy", ".35em")
+ 	      .attr("text-anchor", "middle")
+ 	      .text("Demographic Enrollment")
+ 	      .attr("class","title")
+ 	      ;
 
 		function mouseover() {
 		  d3.select(this).select("path").transition()
@@ -117,13 +117,24 @@ var margin = {
 		        		.attr("d", arcFinal)
 		        		;
 		}
-
-		function up(d, i) {
-					updateBarChart(d.data.category, color(i));
-					updateLineChart(d.data.category, color(i));
-
-		}
 	}
+
+
+d3.json('viz1.json', function(error, data) {
+		d3.select("#donut")
+		.data(data.filter(function(d) {
+			// console.log(d);
+			// console.log(d.Race);
+			donutChartData.push({
+				gender: d.Gender,
+				race: d.Race,
+				category: d.SchoolYear,
+				measure: d.Enrollment
+			});
+			// console.log(donutChartData);
+		}));
+		dsDonutChart();
+		});
 
 // set initial group value
 var group = "All";
@@ -234,7 +245,7 @@ function dsBarChart(genderType) {
   					   		return (i * (width / firstbarChartData.length)) + ((width / firstbarChartData.length - barPadding) / 2);
   					   })
   	  .attr("y", 15)
-  	  .attr("transform","rotate(-45)");
+  	  .attr("transform","rotate(0)");
 
   // Title
   svg.append("text")
@@ -285,5 +296,4 @@ d3.json('viz1.json', function(error, data) {
       // console.log(barChartData);
     }));
 dsBarChart(select_gender);
-
 });
