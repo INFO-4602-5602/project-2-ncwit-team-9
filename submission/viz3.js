e4fc22252790965bbdd57fd99dc12cd5ef34f50d
Ashwin Sankar <ashwinoscar@gmail.com>
2018-03-16 19:53:36 -0600

    s

--- submission/viz3.js
+++ submission/viz3.js
@@ -2,15 +2,15 @@ var margin = {
     top: 20,
     right: 200,
     bottom: 100,
-    left: 50
+    left: 70
   },
   margin2 = {
     top: 430,
     right: 10,
     bottom: 20,
-    left: 40
+    left: 70
   },
-  width = 960 - margin.left - margin.right,
+  width = 1060 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom,
   height2 = 500 - margin2.top - margin2.bottom;
 
